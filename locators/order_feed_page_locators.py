6a1851a85fb5238c114ef18d486a06147b2e0761
dea-1611 <dea.ws1611@gmail.com>
2025-08-18 00:58:08 +0500

    Update order_feed_page_locators.py

--- locators/order_feed_page_locators.py
+++ locators/order_feed_page_locators.py
@@ -4,16 +4,21 @@ from selenium.webdriver.common.by import By
 
 class OrderFeedPageLocators:
 
-    HEADER_FEED_OF_ORDERS = By.XPATH, "//h1[text()='Лента заказов']"            # заголовок "Лента заказов" в хедере
-    ORDER_IN_FEED = By.CSS_SELECTOR, "li.OrderHistory_listItem__2x95r.mb-6"     # лента заказов
-    ORDER_FROM_THE_ORDER_FEED = By.CLASS_NAME, "OrderHistory_listItem__2x95r"   # заказ из ленты заказов
+    # Заголовок "Лента заказов" в хедере
+    HEADER_FEED_OF_ORDERS = By.XPATH, "//h1[text()='Лента заказов']"            
+    
+    # Лента заказов
+    ORDER_IN_FEED = By.CSS_SELECTOR, "li.OrderHistory_listItem__2x95r.mb-6"     
+    
+    # Заказ из ленты заказов
+    ORDER_FROM_THE_ORDER_FEED = By.CLASS_NAME, "OrderHistory_listItem__2x95r"   
 
-    # выполнено заказов за всё время
+    # Выполнено заказов за всё время
     COUNTER_TOTAL_ORDERS_FOR_ALL_TIME = \
         By.XPATH, "//div[p[text()='Выполнено за все время:']]/p[contains(@class, 'OrderFeed_number')]"
-    # выполнено заказов за сегодня
+    # Выполнено заказов за сегодня
     COUNTER_TOTAL_ORDERS_FOR_TODAY = \
         By.XPATH, "//div[p[text()='Выполнено за сегодня:']]/p[contains(@class, 'OrderFeed_number')]"
-    # заказы выполняются сейчас
+    # Заказы выполняются сейчас
     ORDERS_IN_WORK = (By.XPATH,
                       "//ul[@class='OrderFeed_orderListReady__1YFem OrderFeed_orderList__cBvyi']/li[@class='text text_type_digits-default mb-2']")
