a553c12190bc5c08d05b71d49fd3c86b4a605963
dea-1611 <dea.ws1611@gmail.com>
2025-08-18 00:50:31 +0500

    Add files via upload

---
+++ conftest.py
@@ -0,0 +1,36 @@
+import pytest
+import requests
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from helpers import Generators,User
+import allure
+import copy
+from urls import Endpoints
+
+# фикстура запускает браузеры Chrome, Firefox и закрывает их завершению теста
+@pytest.fixture(params=["chrome", "firefox"])
+def driver(request):
+    try:
+        if request.param == "chrome":
+            driver = webdriver.Chrome()
+        elif request.param == "firefox":
+            driver = webdriver.Firefox()
+    except WebDriverException as e:
+        pytest.fail(f"Не удалось запустить браузер {request.param}. Ошибка: {e}")
+    yield driver
+    driver.quit()
+
+
+@pytest.fixture(scope='function')
+def random_user():
+    payload = Generators.generate_payload()
+    with allure.step("Создание пользователя через API"):
+        response = User.register_user(payload)
+        access_token = response.json().get("accessToken")  # Исправлено получение токена
+
+    yield payload["email"], payload["password"]
+
+    with allure.step("Удаление пользователя через API"):
+        if access_token:
+            headers = {"Authorization": f"Bearer {access_token}"}
+            requests.delete(Endpoints.DELETE_USER, headers=headers)
