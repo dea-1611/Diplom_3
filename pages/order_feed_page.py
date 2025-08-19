a5362962c9daa774b7ad308d4c2e4cf48791e4d6
dea-1611 <dea.ws1611@gmail.com>
2025-08-20 00:14:05 +0500

    Update order_feed_page.py

--- pages/order_feed_page.py
+++ pages/order_feed_page.py
@@ -1,6 +1,5 @@
 from pages.base_page import BasePage
 from locators.order_feed_page_locators import OrderFeedPageLocators
-from helpers import *
 import allure
 
 
@@ -9,7 +8,7 @@ class OrderFeedPage(BasePage):
     @allure.step('Получить значение счётчика за всё время')
     def get_counter_value_increases_completed_total(self):
         text = self.get_text_from_element(OrderFeedPageLocators.COUNTER_TOTAL_ORDERS_FOR_ALL_TIME)
-        return int(text.replace(" ", ""))
+        return int(text.replace(" ", ""))  # Удаляем пробелы и преобразуем в число
 
     @allure.step('Получить значение счётчика за сегодня')
     def get_counter_value_increases_completed_total_today(self):
@@ -22,17 +21,34 @@ class OrderFeedPage(BasePage):
 
     @allure.step("Проверяем наличие номера заказа в разделе 'В работе'")
     def check_order_number_in_progress_section_inside_order_feed(self, order_identifier):
-        normalized_identifier = order_identifier.lstrip('0')
-        self.wait.until(
-            lambda driver: any(
-                normalized_identifier in order.text.lstrip('0')
-                for order in driver.find_elements(*OrderFeedPageLocators.ORDERS_IN_WORK)
-            ),
+        normalized_identifier = ''.join(filter(str.isdigit, order_identifier)).lstrip('0')
+
+        def order_in_progress(_):
+            found_orders = self.find_elements(OrderFeedPageLocators.ORDERS_IN_WORK)
+            for order in found_orders:
+                try:
+                    order_text = ''.join(filter(str.isdigit, order.text)).lstrip('0')
+                    if normalized_identifier == order_text:
+                        return True
+                except:
+                    continue
+            return False
+
+        self.wait_until_custom(
+            order_in_progress,
             message=f"Заказ {order_identifier} не появился в разделе 'В работе'"
         )
 
-        orders = self.driver.find_elements(*OrderFeedPageLocators.ORDERS_IN_WORK)
-        order_numbers = [order.text.lstrip('0') for order in orders]
+        orders_elements = self.find_elements(OrderFeedPageLocators.ORDERS_IN_WORK)
+        order_numbers = []
+        for order in orders_elements:
+            try:
+                order_text = ''.join(filter(str.isdigit, order.text)).lstrip('0')
+                order_numbers.append(order_text)
+            except:
+                continue
+
         assert normalized_identifier in order_numbers, (
-            f"Заказ {normalized_identifier} не найден в разделе 'В работе'. Найдены: {order_numbers}"
+            f"Заказ {normalized_identifier} не найден в разделе 'В работе'. "
+            f"Найдены заказы: {order_numbers}"
         )
